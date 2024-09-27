789a2ab614faffad4611a9872040b32d377e945f
suflors <iammagical.space@gmail.com>
2024-09-26 17:07:03 -0700

    Added Series and SeriesParent.

---
+++ src/AtelierTomato.MediaDB.Model/Series.cs
@@ -0,0 +1,13 @@
+﻿namespace AtelierTomato.MediaDB.Model
+{
+	public class Series
+	{
+		public ulong ID { get; set; }
+		public string Name { get; set; }
+		public Series(ulong ID, string name)
+		{
+			this.ID = ID;
+			Name = name;
+		}
+	}
+}
---
+++ src/AtelierTomato.MediaDB.Model/SeriesParent.cs
@@ -0,0 +1,16 @@
+﻿namespace AtelierTomato.MediaDB.Model
+{
+	public class SeriesParent
+	{
+		public ulong ID { get; set; }
+		public ulong ParentID { get; set; }
+		public SeriesParent(ulong ID, ulong parentID)
+		{
+			if (ID == parentID)
+				throw new ArgumentException("A series cannot have a parent that is itself.", nameof(parentID));
+
+			this.ID = ID;
+			ParentID = parentID;
+		}
+	}
+}
