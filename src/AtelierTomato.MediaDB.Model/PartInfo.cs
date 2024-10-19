3a468950c9e53cd7c6328570ff8dea4c366f683a
suflors <iammagical.space@gmail.com>
2024-10-19 14:31:12 -0700

    Added PartInfo.

---
+++ src/AtelierTomato.MediaDB.Model/PartInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace AtelierTomato.MediaDB.Model
+{
+	public class PartInfo
+	{
+		public ulong SeriesID { get; set; }
+		public PartOID? ParentPartID { get; set; }
+		public string PartName { get; set; }
+		public PartInfo(ulong seriesID, PartOID? parentPartID, string partName)
+		{
+			SeriesID = seriesID;
+			ParentPartID = parentPartID;
+			PartName = partName;
+		}
+	}
+}
