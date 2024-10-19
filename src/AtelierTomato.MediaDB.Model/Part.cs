a5867478b95915ef27219efdfdf3ff0d05ca878a
suflors <iammagical.space@gmail.com>
2024-10-19 13:42:06 -0700

    Added Part.

---
+++ src/AtelierTomato.MediaDB.Model/Part.cs
@@ -0,0 +1,15 @@
+﻿namespace AtelierTomato.MediaDB.Model
+{
+	public class Part
+	{
+		public ulong SeriesID { get; set; }
+		public PartOID PartID { get; set; }
+		public string Name { get; set; }
+		public Part(ulong seriesID, PartOID partID, string name)
+		{
+			SeriesID = seriesID;
+			PartID = partID;
+			Name = name;
+		}
+	}
+}
