23a8fbd2e07fc62b84ce470f33640b8cdbd053b0
suflors <iammagical.space@gmail.com>
2024-10-19 14:31:23 -0700

    Added OriginCountries and OriginLanguage to Series.

--- src/AtelierTomato.MediaDB.Model/Series.cs
+++ src/AtelierTomato.MediaDB.Model/Series.cs
@@ -1,13 +1,19 @@
-﻿namespace AtelierTomato.MediaDB.Model
+﻿using System.Globalization;
+
+namespace AtelierTomato.MediaDB.Model
 {
 	public class Series
 	{
 		public ulong ID { get; set; }
 		public string Name { get; set; }
-		public Series(ulong ID, string name)
+		public IReadOnlyList<RegionInfo> OriginCountries { get; set; } = [];
+		public CultureInfo? OriginLanguage { get; set; } = null;
+		public Series(ulong ID, string name, IReadOnlyList<RegionInfo>? originCountries = null, CultureInfo? originLanguage = null)
 		{
 			this.ID = ID;
 			Name = name;
+			OriginCountries = originCountries ?? [];
+			OriginLanguage = originLanguage;
 		}
 	}
 }
