b2800aa11e085d817c9118c662fc89db130f1c00
suflors <iammagical.space@gmail.com>
2024-11-26 21:50:19 -0800

    Implemented StatisticsPanel.

---
+++ src/AtelierTomato.MediaDB.Client/Panels/Models/MediaDBStats.cs
@@ -0,0 +1,12 @@
+﻿namespace AtelierTomato.MediaDB.Client.Panels.Models
+{
+	public record MediaDBStats(
+		int Series,
+		int SeriesNames,
+		int SeriesParentPairs,
+		int Parts,
+		int PartNames,
+		int PartGroupInfos,
+		int PartGroupNames
+	);
+}
--- src/AtelierTomato.MediaDB.Client/Panels/StatisticsPanelViewModel.cs
+++ src/AtelierTomato.MediaDB.Client/Panels/StatisticsPanelViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel;
+﻿using AtelierTomato.MediaDB.APIClient;
+using AtelierTomato.MediaDB.Client.Panels.Models;
+using System.ComponentModel;
 using System.Composition;
+using System.Windows.Input;
 
 namespace AtelierTomato.MediaDB.Client.Panels
 {
@@ -7,6 +10,44 @@ namespace AtelierTomato.MediaDB.Client.Panels
 	[Shared]
 	public class StatisticsPanelViewModel : INotifyPropertyChanged
 	{
+		private RestClient _client;
+
 		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public ICommand GetStatisticsCommand { get; }
+		private MediaDBStats? mediaDBStats;
+		public MediaDBStats? MediaDBStats
+		{
+			get => mediaDBStats;
+			set
+			{
+				if (mediaDBStats != value)
+				{
+					mediaDBStats = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MediaDBStats)));
+				}
+			}
+		}
+
+		[ImportingConstructor]
+		public StatisticsPanelViewModel(RestClient client)
+		{
+			_client = client;
+			GetStatisticsCommand = new DelegateCommand(GetStatistics);
+		}
+
+		private async void GetStatistics()
+		{
+			MediaDBStats = new MediaDBStats
+			(
+				await _client.CountSeriesAsync(),
+				await _client.CountSeriesNamesAsync(),
+				await _client.CountSeriesParentsAsync(),
+				await _client.CountPartsAsync(),
+				await _client.CountPartNamesAsync(),
+				await _client.CountPartGroupInfoAsync(),
+				await _client.CountPartGroupNamesAsync()
+			);
+		}
 	}
 }
